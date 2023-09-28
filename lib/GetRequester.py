4cf2fffec79b2702186b102120914385225b3f62
mutembeic <135004069+mutembeic@users.noreply.github.com>
2023-09-28 06:54:37 +0300

    done

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,26 @@ import requests
 import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        try:
+            response = requests.get(self.url)
+            response.raise_for_status()
+            return response.content  
+        except requests.exceptions.RequestException as e:
+            print(f"Error: {e}")
+            return None
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        if response_body is not None:
+            try:
+                json_data = json.loads(response_body.decode('utf-8'))  # Decode bytes and parse JSON
+                return json_data
+            except ValueError as e:
+                print(f"Error parsing JSON: {e}")
+                return None
+        else:
+            return None
